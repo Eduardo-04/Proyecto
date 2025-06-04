f51ac4ff1569550a5f28d46bd518a966561416b6
Edray <edraivelasquez@gmail.com>
2025-06-04 12:27:23 -0600

    primer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 p-8">
       <h1 className="text-3xl font-bold text-center text-purple-600">
-        ¡Proyecto funcionando perfectamente! 🌟
+        ¡Proyecto funcionando perfectamente!hola como estas 🌟
       </h1>
     </div>
   );
